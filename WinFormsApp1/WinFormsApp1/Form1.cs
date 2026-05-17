674e5da1048b1357bf6e0ee861f6cb8f8988e867
sajik <sajiki.mune08419@gmail.com>
2026-05-17 12:28:31 +0900

    first commit

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
